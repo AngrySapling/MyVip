2091abdf1bec2acd466a62017c3fa6674064c753
AngrySapling <liwei_lm@163.com>
2018-12-06 17:00:09 +0800

    我的会员

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <router-view></router-view>
 </template>
@@ -22,7 +21,7 @@ export default{
             this.axios.post(url,params).then(function(res){
                 let url = res.data.result;
                 let openid = Cookies.get('OpenID');
-                window.location.href=url;//跳转到验证登录链接
+                // window.location.href=url;//跳转到验证登录链接
             })
         }
     },
@@ -39,46 +38,3 @@ export default{
 <style>
 </style>
 
-=======
-<template>
-    <router-view></router-view>
-</template>
-<script>
-import Cookies from 'js-cookie'
-export default{
-    data(){
-        return{
-        }
-    },
-    methods:{
-        //会员验证
-        IsVip(){
-            console.log(this.$store.state.webUrl,666)
-            let url = this.$store.state.url;
-            let params = JSON.stringify({
-                "id":14,
-                "method":"/MainSystem/Q3ZhangYiYuan/Rpcs/WeixinMPRpc/GetWeixinAuthUrl",
-                "params":[this.$store.state.webUrl+'#/loading',]
-            })
-            this.axios.post(url,params).then(function(res){
-                let url = res.data.result
-                let openid = Cookies.get('OpenID');
-                window.location.href=url;//跳转到验证登录链接
-            })
-        }
-    },
-    mounted(){
-        let openid = Cookies.get('OpenID');
-        if(openid !== undefined){
-            this.$router.push('/index/message')
-        }else{
-            this.IsVip();
-        }
-        
-    },
-}
-</script>
-<style>
-</style>
-
->>>>>>> ecb574a033bf09a448adac36df006b8655842ff1
